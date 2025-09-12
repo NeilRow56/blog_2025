9e1858d004995e887546a694662c039860822208
NeilRow56 <neilrowland56@gmail.com>
2025-09-12 07:19:21 +0100

    Final tweaks to drizzle and better auth install

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,29 @@
+import { db } from '@/db'
+import { schema } from '@/db/schema'
 import { betterAuth } from 'better-auth'
-
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { nextCookies } from 'better-auth/next-js'
 export const auth = betterAuth({
   emailAndPassword: {
-    enabled: true
-  }
+    enabled: true,
+    requireEmailVerification: false
+  },
+  session: {
+    expiresIn: 30 * 24 * 60 * 60 * 2, // 60 days - default is 7 days
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60
+    }
+  },
+
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    schema: {
+      ...schema,
+      user: schema.users,
+      session: schema.sessions,
+      accounts: schema.accounts
+    }
+  }),
+  plugins: [nextCookies()]
 })
